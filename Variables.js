1bd04a9231a61ac10331b788807352c450e52928
PhrastusX <56659535+PhrastusX@users.noreply.github.com>
2020-01-19 12:07:36 -0700

    finished

---
+++ Variables.js
@@ -0,0 +1,18 @@
+// let sets a variable, can be strings ints or floats. let is for any variable that we want to change.
+let age = 28 //one = sign sets the variable
+
+console.log(age); //output is 28
+
+age = age + 1 //takes the variable age and sets it equal to age plus one
+
+console.log(age)//outputs 29
+
+age += 1 // does the same thing as age = age + 1; can use this format for - * /
+
+console.log(age)//outputs 30
+
+const pi = 3.1415 //const is used when we do not want a value to change.
+
+console.log(pi) 
+
+// If I tried to have pi = "3.14" javascript would throw an error
